b5f9bb80a2d96a958f644d8abe9b07b92feb9d8f
Adam Wieckert <wiec1369@gmail.com>
2018-05-24 21:23:31 -0500

    Things n Stuff

---
+++ javascripts/events.js
@@ -0,0 +1,20 @@
+const data = require('./data.js');
+
+const addSearchEvent = () => {
+  $('#search').on('keypress', searchWeather);
+};
+
+const searchWeather = (e) => {
+  const inputValue = $('#search').val();
+  if (e.key === 'Enter' && data.validateSearch(inputValue)) {
+    // call promise function to grab info from API
+    alert('YAY in the if');
+
+  } else if (e.key === 'Enter' && !data.validateSearch(inputValue)) {
+    alert('Not a valid Zip');
+  }
+};
+
+module.exports = {
+  addSearchEvent,
+};
